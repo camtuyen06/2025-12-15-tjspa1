a1e439e8a92dadf3c48b21064b71b0b008df8562
camtuyen06 <tcttran06@gmail.com>
2025-12-15 15:08:48 +0100

    ajoute prop bgColor

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,12 +6,13 @@ function App() {
       DEMAT BREIZH
       <br />
          <Button title={"benjamin"}
+            bgColor="tomato"
             onButtonClick={(param)=>{
             console.log("j'ai ckiqué sur benjamin", param)
          }}
       />
 
-      <Button title="ok"
+      <Button title="ok" bgColor="aquamarine"
        onButtonClick={(param)=>{
             console.log("j'ai ckiqué sur ok", param)
          }}></Button>
