32ab1e5be6d6b6e1d0fa9b58bba0140b3b5dbc86
camtuyen06 <tcttran06@gmail.com>
2025-12-17 10:53:38 +0100

    modif store appelant ressourcesReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,43 +1,15 @@
 import {configureStore } from "@reduxjs/toolkit";
-import { emptyMeme }  from "orsys-tjs-meme";
+import ressourcesReducer from "./ressources";
+import currentReduccer from "./current";
 
 
-const initialState = {
-  meme: emptyMeme
-};
 
-const reducer = (state = initialState, action) => {
-  console.log(action);
-
-  switch (action.type) {
-    case "update":
-      return { ...state, meme: { ...state.meme, ...action.payload } };
-    case "clear":
-      return { ...state, meme: initialState.meme };
-    default:
-      return state;
-  }
-};
-const update=(memeContent)=>({ type: "update", payload: memeContent })
-
-// let stateb = reducer(undefined, { type: "@@INIT" });
-// console.log(stateb);
-// stateb = reducer(state,update( { id: 0, text: "coucou" }));
-// console.log(stateb);
-// stateb = reducer(state, { type: "update", payload: { id: 0, text: "toto" } });
-// console.log(stateb);
-// stateb = reducer(state, { type: "clear" });
-// console.log(stateb);
-
-// const store=createStore(reducer);
-const store=configureStore({reducer:reducer})
+const store=configureStore({
+    reducer:{ressources: ressourcesReducer, current:currentReduccer}
+})
 store.subscribe(()=>{
     console.log(store.getState());
     
 })
-store.dispatch(update( { id: 0, text: "coucou" }));
 
-store.dispatch(update( { text: "toto" }));
-store.dispatch(update( { text: "tata" }));
-store.dispatch(update( { text: "totoro" }));
  
